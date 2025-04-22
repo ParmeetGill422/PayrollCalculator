fb91d927ca3ba01ad1fa90699d5c248a88a48850
Parmeet Gill <parmeetgill422@gmail.com>
2025-04-22 14:21:36 -0700

    Initial commit

---
+++ src/Employee.java
@@ -0,0 +1,44 @@
+public class Main {
+
+
+    private int employeeId;
+    private String name;
+    private double hoursWorked;
+    private double payRate;
+
+    // Parameterized constructor
+    public Main(int employeeId, String name, double hoursWorked, double payRate) {
+        this.employeeId = employeeId;
+        this.name = name;
+        this.hoursWorked = hoursWorked;
+        this.payRate = payRate;
+    }
+
+    // Getters
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getHoursWorked (){
+        return hoursWorked;
+    }
+    public double getPayRate (){
+        return payRate;
+    }
+    public void setName (String name){
+        this.name = name;
+    }
+    public void setHoursWorked(double hoursWorked){
+        this.hoursWorked = hoursWorked;
+    }
+    public void setPayRate (double payRate){
+        this.payRate = payRate;
+    }
+    public double getGrossPay(){
+        return hoursWorked * payRate;
+    }
+}
